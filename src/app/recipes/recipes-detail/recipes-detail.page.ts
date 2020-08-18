5bac14c5baeb5b1dc8126af226f8a0507fbd5364
hardik <hardiksandip@gmail.com>
2020-08-18 12:40:27 +0530

    Alert controller and Action Sheet controler demo

--- src/app/recipes/recipes-detail/recipes-detail.page.ts
+++ src/app/recipes/recipes-detail/recipes-detail.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { RecipesService } from '../recipes.service';
 import { Recipe } from '../recipe.modal';
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-recipes-detail',
@@ -14,7 +15,8 @@ export class RecipesDetailPage implements OnInit {
   constructor(
     private activatedRoute: ActivatedRoute,
     private recipesService: RecipesService,
-    private router: Router
+    private router: Router,
+    private altctr: ActionSheetController
   ) {}
 
   ngOnInit() {
@@ -32,8 +34,24 @@ export class RecipesDetailPage implements OnInit {
     });
   }
 
-  onDeleteClick() {
-    this.recipesService.deleteRecipe(this.loadedRecipe.id);
-    this.router.navigate(['recipes']);
+  async onDeleteClick() {
+    const alert = await this.altctr.create({
+      header: 'Are you sure?',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+        },
+        {
+          text: 'Delete',
+          role: 'destructive',
+          handler: () => {
+            this.recipesService.deleteRecipe(this.loadedRecipe.id);
+            this.router.navigate(['recipes']);
+          },
+        },
+      ],
+    });
+    await alert.present();
   }
 }
